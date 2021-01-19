9a534265ccabd9085435c1238c5ee3ac3155ba55
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-01-19 14:51:18 +0100

    Add files via upload

--- bord.js
+++ bord.js
@@ -4,7 +4,7 @@ var Check = document.getElementById("Check");
 var text = document.getElementById("Text");
 var input = Check.value;
 var word1 = words[Math.round(Math.random() * words.length)];
-
+console.log(word1);
 var container2 = document.getElementById("container2");
 
 const aantal_rijen = 5;
@@ -69,30 +69,37 @@ Check.addEventListener("keyup", function(event){
 //Check the user input
 function CheckInput() {
   input = Check.value;
-  /*console.log(CheckInput);*/
-  if(input.length == word1.length) {
+  if(rij_nummer > aantal_rijen){
+    alert('U heeft geen pogingen meer om te raden!'); 
+
+  }else if(input.length != word1.length){
+    alert('Geen geldige invoer! Alleen '+ word1.length+' letter woorden zijn toegestaan.'); 
+
+  /*}else if(input.length == null){
+    alert("Ongeldige poging! Voer een woord in.");*/
+
+  }else if(input.length == word1.length) {
     for (var i=0; i<word1.length; i++){
       var letter2 = word1.charAt(i);
       var letter = input.charAt(i);
 
       document.getElementById("letter" + (i + 1) + "rij" + rij_nummer).innerHTML = letter;
-
-      //controleren op letters/kleuren
       if(input == word1){
-
-      }else{
-
+        document.getElementById("letter" + (i + 1) + "rij" + rij_nummer).style.backgroundColor= "green";
       }
     }
-
-    rij_nummer++;
-  } else if (input.length != word1.length){
-    alert('Geen geldige invoer! Alleen '+ word1.length+' letter woorden zijn toegestaan.'); 
-  }else if (rij_nummer >= 5){
-    alert('U heeft geen pogingen meer om te raden!'); 
   }
+  for (var i = 0; i < word1.length; i++) {
+    
+    // for (var a = 0; a< word1.length; a++) {
+    //   if(word1 != "" && word1 == input){
+    //     document.getElementById("letter" + (i + 1) + "rij" + rij_nummer).style.backgroundColor = "yellow";
+    //   }
+    // }
+  }
+  rij_nummer++;
 }
-
+  
 //Show first letter
 ShowLetter(0);
 function ShowLetter(index_letter){
